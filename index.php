c853831219bb9b3fca94984d26c1057314a6b603
gmgerstner <gmgerstner@gmail.com>
2016-12-02 21:28:14 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
-
+	include 'resize.image.class.php';
+	$image = new Resize_Image;
+	$image->new_width = 200;
+	$image->new_height = 200;
+	$image->image_to_resize = "/home/mysite.com/public_html/images/sunset_wallpaper.jpg"; // Full Path to the file
+	$image->ratio = true; // Keep Aspect Ratio?
+	// Name of the new image (optional) - If it's not set a new will be added automatically
+	$image->new_image_name = 'sunset_wallpaper_thumbnail';
+	/* Path where the new image should be saved. If it's not set the script will output the image without saving it */
+	$image->save_folder = 'thumbs/';
+	$process = $image->resize();
+	if($process['result'] && $image->save_folder)
+	{
+		echo 'The new image ('.$process['new_file_path'].') has been saved.';
+	}
 ?>
